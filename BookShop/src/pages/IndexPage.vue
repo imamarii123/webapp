3ca1542c90c57183d0024787fbe99876c5e9d286
imamarii123 <imamarii123@gmail.com>
2025-11-27 08:32:33 +0100

    Inicijalizacija quasar projekta i dodavanje podatci u indexpage

---
+++ BookShop/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    <div>
+      <p>Ime i prezime: Iva Marjanović</p>
+      <p>Datum:21.11.2021 </p>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
